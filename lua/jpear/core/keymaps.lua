1d89e2c0e379aeb3e31c83d3d1f5122c30091784
Jypear <josh_jip_pearson@hotmail.com>
2024-07-22 22:26:32 +0100

    Add nvim-config

---
+++ lua/jpear/core/keymaps.lua
@@ -0,0 +1,32 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- concise
+
+-- Exit maps
+keymap.set("i", "jj", "<ESC>")
+
+-- Tab Maps
+keymap.set("n", "<leader>n", ":tabnew<CR>") -- New blank tab
+keymap.set("n", "<leader>w", ":tabclose<CR>") -- tab close - Note close splits first
+keymap.set("n", "<leader>f", ":tabn<CR>") -- Move next tab 
+keymap.set("n", "<leader>d", ":tabp<CR>") -- Move previous tab
+
+-- Split maps
+keymap.set("n", "<leader>s", "<C-w>v") -- split window
+keymap.set("n", "<leader>w", ":close<CR>") -- close window
+keymap.set("n", "<leader>=", "<C-w>=") -- Make split equal
+keymap.set("n", "<leader>h", "<C-w>h") -- Move to left window
+keymap.set("n", "<leader>l", "<C-w>l") -- Move to right window
+
+-- highlight move
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Center moving
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "j", "jzz")
+keymap.set("n", "k", "kzz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
---
+++ lua/jpear/core/options.lua
@@ -0,0 +1,34 @@
+local opt = vim.opt -- sets vim as variable - more concise
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs/indents
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- apearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+opt.clipboard:append("unnamedplus")
+
+-- splits
+opt.splitright = true
+opt.splitbelow = true
