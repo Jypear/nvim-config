3512d1468a64afde5fbe6e49c3c32e1ea1060de0
Jypear <josh_jip_pearson@hotmail.com>
2024-07-22 22:46:11 +0100

    Add indent blankline

--- lua/jpear/plugins.lua
+++ lua/jpear/plugins.lua
@@ -6,7 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
+  use "lukas-reineke/indent-blankline.nvim"
   use {
       'nvim-telescope/telescope.nvim', tag = '0.1.6',
       -- or                            , branch = '0.1.x',
