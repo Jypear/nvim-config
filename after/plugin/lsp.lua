3e918eb9ce7ea8f329345db5ccbe077cacf96c07
Jypear <josh_jip_pearson@hotmail.com>
2024-07-23 22:31:00 +0100

    Add auto complete for lsp menu

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,6 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -6,11 +8,30 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+lsp_zero.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 10000
+    },
+    servers = {
+        ['ruff_lsp'] = {'python'}
+    }
+})
+
+cmp.setup({
+    mapping = {
+        ['<C-Tab>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping.confirm({select=true}),
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+    }
+})
+
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = {'ruff_lsp', 'ruff', 'pyright'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
